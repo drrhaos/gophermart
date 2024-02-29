0670c09cdadd10aa4c19a99abb3e6bd28828e062
user <user@example.com>
2024-02-29 22:25:50 +0300

    init

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"os"
+
+	"gophermart/internal/logger"
+)
+
+const urlPostUserRegister = "/api/user/register"                // регистрация пользователя
+const urlPostUserLogin = "/api/user/login"                      // аутентификация пользователя;
+const urlPostUserOrders = "/api/user/orders"                    // загрузка пользователем номера заказа для расчёта;
+const urlGetUserorders = "/api/user/orders"                     // получение списка загруженных пользователем номеров заказов, статусов их обработки и информации о начислениях;
+const urlGetUserBalance = "/api/user/balance"                   // получение текущего баланса счёта баллов лояльности пользователя;
+const urlPostUserBalanceWithdraw = "/api/user/balance/withdraw" // запрос на списание баллов с накопительного счёта в счёт оплаты нового заказа;
+const urlGetUserWithdrawals = "/api/user/withdrawals"           // получение информации о выводе средств с накопительного счёта пользователем.
+
+var cfg Config
+
+func main() {
+	logger.Init()
+	ok := cfg.readStartParams()
+	if !ok {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module gophermart
+
+go 1.21.0
+
+require (
+	github.com/caarlos0/env/v10 v10.0.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
